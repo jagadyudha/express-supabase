2663f5daba9e1f25588a232b0d783660ffeb7610
zero0jef <jefryyfernandoo@gmail.com>
2022-08-08 18:24:10 +0700

    change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const supabase = require("./route/supabase");
 const app = express();
-const port = 3000;
+const port = 4000;
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
