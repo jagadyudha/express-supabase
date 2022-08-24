1e75b088c53edf05f068995026d1b4d107841a49
zero0jef <jefryyfernandoo@gmail.com>
2022-08-24 15:25:47 +0700

    transaksi

--- index.js
+++ index.js
@@ -83,7 +83,7 @@ app.get("/layanan", async (req, res) => {
 app.get("/transaksi", async (req, res) => {
   const { data, error } = await supabase.from("transaksi").select(`
     id_transaksi, tgl_masuk, tgl_keluar, kendaraan, status, total_harga,
-    user:id_user (id_user), layanan:id_layanan (id_layanan)
+    user:id_user (id_user), layanan:id_layanan (id_layanan, nama_layanan, deskripsi_layanan)
   `);
   res.status(200).json(data);
 });
